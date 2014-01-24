88c34afcb1216df1a00db2a59db7eac399386c84
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-01-24 21:42:24 +0100

    machine stats and sort

--- app/controllers/background.rb
+++ app/controllers/background.rb
@@ -2,7 +2,8 @@ HelloServer::App.controller :background do
   post :refresh, csrf_protection: false, provides: [:js] do
     CurrentMachine.refresh
 
-    @services = HelloServerClient::Service.all
+    # holy shit!
+    @services = HelloServerClient::Service.all.collect { |s| s }.sort { |a, b| a.name.to_s <=> b.name.to_s }
     render 'background/index'
   end
 end
--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -1,6 +1,7 @@
 HelloServer::App.controller :dashboard do
   get :index, :provides => [:html, :rss, :atom] do
-    @services = HelloServerClient::Service.all
+    # holy shit!
+    @services = HelloServerClient::Service.all.collect { |s| s }.sort { |a, b| a.name.to_s <=> b.name.to_s }
     render 'dashboard/index'
   end
 end
