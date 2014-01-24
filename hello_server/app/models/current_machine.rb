88d0f173dc916ac64c58a73847e18d3d51ee5c3b
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-01-24 15:32:06 +0100

    polishing

--- hello_server/app/models/current_machine.rb
+++ hello_server/app/models/current_machine.rb
@@ -6,11 +6,25 @@ class CurrentMachine
     usw = Usagewatch
     s.value = {
       cpu: {
-        usage: usw.uw_cpuused
+        cpu_usage: usw.uw_cpuused,
+        load: usw.uw_load,
+      },
+      memory: {
+        usage: usw.uw_memused.to_s + "%",
       },
       hdd: {
         disk_usage: usw.uw_diskused,
-      }
+        diskioreads: usw.uw_diskioreads,
+        diskiowrites: usw.uw_diskiowrites,
+      },
+      network: {
+        tcp_usage: usw.uw_tcpused,
+        udp_usage: usw.uw_udpused,
+        bandrx: usw.uw_bandrx,
+        bandtx: usw.uw_bandtx
+      },
+      cpu_top: usw.uw_cputop.collect{|top| "#{top[1]}% - #{top[0]}"},
+      mem_top: usw.uw_memtop.collect{|top| "#{top[1]}% - #{top[0]}"},
     }
     s.save!
     s
