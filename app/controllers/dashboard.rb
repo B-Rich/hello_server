eece0e374e29a805b1719048dabba5bb095bf99a
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-01-25 16:29:25 +0100

    clean old models

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -4,4 +4,14 @@ HelloServer::App.controller :dashboard do
     @services = HelloServerClient::Service.all.collect { |s| s }.sort { |a, b| a.name.to_s <=> b.name.to_s }
     render 'dashboard/index'
   end
+
+  delete :clean, csrf_protection: false do
+    HelloServerClient::Service.all.each do |s|
+      if s.updated_at < (Time.now - 60*60)
+        s.delete
+      end
+    end
+    redirect_to '/dashboard'
+  end
+
 end
