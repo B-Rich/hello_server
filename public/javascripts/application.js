8104a4451e81dde0daa1870839274302bbefcc42
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-01-26 20:01:51 +0100

    rename service to notification

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,15 @@
 // Put your application scripts here
-function getIntervalServices() {
+function getIntervalNotifications() {
   var _interval = $("input[name='refresh_interval']").val();
 
   $.ajax({
     type: "POST",
     url: "/background/refresh",
     success: (function (data) {
-      $("#services").effect("highlight", {}, 800);
+      $("#notifications").effect("highlight", {}, 800);
     })
   }).done(function () {
       console.log("Refreshing in " + _interval);
-      setTimeout(getIntervalServices, _interval);
+      setTimeout(getIntervalNotifications, _interval);
     });
 }
